c586aaf524101d41d08f03d5c26528fe92733b1a
MohamedKandel <mohamed.hossam29699@gmail.com>
2024-08-24 11:55:34 +0300

    replace gradle with version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,69 +42,62 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity:1.9.1")
-    val room_version = "2.6.1"
+    implementation(libs.androidx.activity)
 
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.recyclerview)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 
     // page indicator
-    implementation("com.tbuonomo:dotsindicator:5.0")
+    implementation(libs.dotsindicator)
 
     // navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
 
     // retrofit
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-scalars:2.9.0")
-    implementation ("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation ("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
+    implementation (libs.converter.scalars)
+    implementation (libs.okhttp)
+    implementation (libs.logging.interceptor)
 
     // viewModel and lifecycle
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.8.4")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
+    implementation (libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
     //coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation(libs.kotlinx.coroutines.android)
 
     // ssp and sdp
-    implementation("com.intuit.ssp:ssp-android:1.1.1")
-    implementation("com.intuit.sdp:sdp-android:1.1.1")
+    implementation(libs.ssp.android)
+    implementation(libs.sdp.android)
 
     // shared prefs
-    implementation("androidx.preference:preference-ktx:1.2.1")
+    implementation(libs.androidx.preference.ktx)
 
     // get connected device
-    implementation ("com.github.tejmagar:AndroidNetworkTools:1.0.2alpha")
-    implementation ("com.github.tejmagar:AndroidWiFiTools:1.0.2")
+    implementation (libs.androidnetworktools)
+    implementation (libs.androidwifitools)
 
     // in app update
-    implementation("com.google.android.play:app-update:2.1.0")
-    implementation("com.google.android.play:app-update-ktx:2.1.0")
+    implementation(libs.app.update)
+    implementation(libs.app.update.ktx)
 
     // room db
-    annotationProcessor("androidx.room:room-compiler:$room_version")
-    implementation("androidx.room:room-runtime:$room_version")
-    implementation("androidx.room:room-ktx:$room_version")
-    ksp("androidx.room:room-compiler:$room_version")
+    annotationProcessor(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
 
     // payment
-    implementation("com.braintreepayments.api:google-pay:4.39.0")
-    implementation("com.braintreepayments.api:data-collector:4.39.0")
-
-    // recyclerview swipe decorator
-//    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation(libs.google.pay)
+    implementation(libs.data.collector)
 
     // gif
-    implementation("pl.droidsonroids.gif:android-gif-drawable:1.2.29")
-
-    // count edit text
-//    implementation("com.github.Damercy:IncrementDecrementButton:2.0.0")
+    implementation(libs.android.gif.drawable)
 }
