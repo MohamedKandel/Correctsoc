ef5faa91242d50f26ce42f06fc53cae748b06637
MohamedKandel <mohamed.hossam29699@gmail.com>
2024-09-22 15:31:08 +0300

    update port for APIs

--- app/src/main/java/com/correct/correctsoc/helper/Constants.kt
+++ app/src/main/java/com/correct/correctsoc/helper/Constants.kt
@@ -5,7 +5,7 @@ object Constants {
     const val LANG = "lang"
     const val FIRST_TIME = "first start"
     const val IP_ADDRESS = "ip address"
-    const val BASE_URL = "https://correctsoc.com:8443/"
+    const val BASE_URL = "https://correctsoc.com:7/"
     const val VENDOR_BASE_URL = "https://api.macvendors.com/"
     const val EXTERNAL_IP = "http://api.ipify.org/"
     const val RESERVE_EXTERNAL_IP = "http://ifcfg.me/"
